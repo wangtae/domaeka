76ae299a23d0934085d49be369816af0ce3cc2ae
wangtae@gmail.com <wangtae@gmail.com>
2025-07-09 19:42:36 +0900

    x

--- web/config.php
+++ web/config.php
@@ -33,7 +33,7 @@ date_default_timezone_set("Asia/Seoul");
 보안서버주소가 없다면 공란으로 두시면 되며 보안서버주소 뒤에 / 는 붙이지 않습니다.
 입력 예) https://www.domain.com:443/gnuboard5
 */
-define('G5_DOMAIN', 'http://localhost:8001');
+define('G5_DOMAIN', 'http://domaeka.local');
 define('G5_HTTPS_DOMAIN', '');
 
 // 그누보드 디버그바 설정입니다, 실제 서버운영시 false 로 설정해 주세요.
@@ -55,7 +55,7 @@ define('G5_DB_CHARSET', 'utf8');
 www.sir.kr 과 sir.kr 도메인은 서로 다른 도메인으로 인식합니다. 쿠키를 공유하려면 .sir.kr 과 같이 입력하세요.
 이곳에 입력이 없다면 www 붙은 도메인과 그렇지 않은 도메인은 쿠키를 공유하지 않으므로 로그인이 풀릴 수 있습니다.
 */
-define('G5_COOKIE_DOMAIN',  'localhost');
+define('G5_COOKIE_DOMAIN',  'domaeka.local');
 
 define('G5_DBCONFIG_FILE',  'dbconfig-domaeka.php');
 
